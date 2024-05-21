3f67b5c6bf136186ee17b99be5a4ac99f5be496b
David Hankin <d_hankin@outlook.com>
2024-05-21 18:17:44 +0200

    update text

--- src/experience/Experience.tsx
+++ src/experience/Experience.tsx
@@ -11,30 +11,28 @@ function Experience() {
               <td><h5>2023-</h5></td>
               <td>
                   <h3>Postnord AB<br/>Produktionschef</h3>
-                  <p>Ansvar för paketterminalens produktion dagligen men även långsiktig med tanke på stora projekt och händelse. Ansvar för sex arbetsledare.</p>
+                  <p>Ansvar för paketterminalens produktion dagligen men även långsiktig med tanke på stora projekt och händelser. Ansvar för sex arbetsledare med sammanlagt ca 80 arbetare.</p>
               </td>
             </tr>
             <tr>
               <td><h5>2018-2022</h5></td>
               <td>
                   <h3>Postnord AB<br/>Processledare</h3>
-                  <p>Mer fokus på företagets logistiska processer. Ansvar för planering och tillsyn av ankomst och avgång av gods från terminalen. Kontakt med företagskunder 
-                    för att planera ankomster eller lösa logistiska problem. Säkerhetsansvar med fokus på förare, arbetsplats och logistiska frågor.</p>
+                  <p>Fokus på företagets logistiska processer. Ansvar för planering och tillsyn av ankomst och avgång av gods till och från terminalen. Kontakt med företagskunder för att planera ankomster eller lösa logistiska problem. Säkerhetsansvar med fokus på förare, arbetsplats och logistiska frågor.</p>
               </td>
             </tr>
             <tr>
               <td><h5>2012-2018</h5></td>
               <td>
                   <h3>Postnord AB<br/>Produktionsledare</h3>
-                  <p>Planering och genomförande av den dagliga produktionen på paketterminalen med fokus på personal men även överblick över de logistiska processerna.
-                    Ansvarig för ett 70-tal arbetare dagligen tillsammans med fyra andra.</p>
+                  <p>Planering och genomförande av den dagliga produktionen på paketterminalen med fokus på personal men även överblick över de logistiska processerna. Ansvarig för ett 70-tal arbetare dagligen tillsammans med fyra andra.</p>
               </td>
             </tr>
             <tr>
               <td><h5>2010-2012</h5></td>
               <td>
                   <h3>Postnord AB<br/>Terminalarbetare</h3>
-                  <p>Mitt första jobb efter flytten till Sverige. Mycket fysiskt arbete där man lossade och lastade släp.</p>
+                  <p>Mitt första arbete efter flytten till Sverige. Mycket fysiskt arbete där man lossade och lastade släp.</p>
               </td>
             </tr>
           </tbody>
@@ -75,9 +73,7 @@ function Experience() {
               <td><h5>2012-2016</h5></td>
               <td>
                   <h3>The Open University<br/>Högskoleexamen: Astrofysik</h3>
-                  <p>En tvåårig kurs studerad i 50% takt samtidigt som jag arbetade heltid.
-                    Avancerad matematik och fysik med fokus på astrofysik.
-                    Det slutliga projektet var att fastställa ljuskällan för ett oidentifierat pulserande himlaobjekt med fjärråtkomst till ett teleskop i Tyskland.</p>
+                  <p>En tvåårig kurs studerad i 50% takt samtidigt som jag arbetat heltid. Avancerad matematik och fysik med fokus på astrofysik. Det slutliga projektet var att fastställa ljuskällan för ett oidentifierat pulserande himlaobjekt med fjärråtkomst till ett teleskop i Tyskland.</p>
               </td>
             </tr>
           </tbody>
